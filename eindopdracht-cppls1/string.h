cfba13b07e06a4037ca98db3643738a3204ad00b
Jorrit <Jorrit-93@hotmail.com>
2019-10-16 13:55:11 +0200

    Fixed string

--- eindopdracht-cppls1/string.h
+++ eindopdracht-cppls1/string.h
@@ -7,15 +7,20 @@ class String
 	
 private:
 	char* n_string;
-	short n_length;
+	int n_length;
 
 public:
 	String();
-	String(const char* input);
-	String(const String& s);
 	~String();
 
-	String& operator=(const String& s);
+	//copy
+	String(const String& other);
+	String& operator=(const String& other);
+
+	//move
+	String(const char* input) noexcept;
+	String& operator=(const char* input) noexcept;
+
 	friend bool operator==(const String& s1, const String& s2);
 
 	friend std::ostream& operator<<(std::ostream& os, const String& s);
