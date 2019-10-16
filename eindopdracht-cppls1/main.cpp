7c80f99de8c9ebbc437ce1784186a5d1750b740d
Jorrit <Jorrit-93@hotmail.com>
2019-10-16 18:16:36 +0200

    cleaned main

--- eindopdracht-cppls1/main.cpp
+++ eindopdracht-cppls1/main.cpp
@@ -1,17 +1,6 @@
 #include <iostream>
-#include "list.h"
-#include "dictionary.h"
-#include "cannon.h"
-#include "light_cannon.h"
-#include "heavy_cannon.h"
-#include "medium_cannon.h"
-#include "ship_builder.h"
 
 int main()
 {
-	auto attributes = new Array<ShipAttribute>(2);
-	attributes->add(ShipAttribute::Small);
-	attributes->add(ShipAttribute::Light);
-	auto test = ShipBuilder().setType(ShipType::Brigg).setPrice(27000).setCargoSpace(250).setCannonAmount(16).setHP(140).setAttributes(attributes).build();
 	std::cout << "" << "\n";
 }
